90b92435f64b87ec5d92f84f1180b3f2548e9487
CaueGrande <111818673+CaueGrande@users.noreply.github.com>
2023-06-14 20:45:49 -0300

    Update Fase.java

--- Jogo_Caue/src/br/ifpr/jogo/modelo/Fase.java
+++ Jogo_Caue/src/br/ifpr/jogo/modelo/Fase.java
@@ -3,28 +3,45 @@ package br.ifpr.jogo.modelo;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 import br.ifpr.jogo.modelo.Inimigos.Lobo;
 
-public class Fase extends JPanel{
+public class Fase extends JPanel implements KeyListener, ActionListener{
     private Image fundo;
     private Personagem personagem;
     private Lobo lobo;
+    private Timer timer;
+
+    private static final int DELAY = 5;
+    private static final int VELOCIDADE_DESLOCAMENTO = 3;
     
     
     public Fase(){
+        this.setFocusable(true);
+        this.setDoubleBuffered(true);
+
         ImageIcon carregando = new ImageIcon("recursos\\fundo.png");
         
         this.fundo = carregando.getImage();
 
-        this.personagem = new Personagem();
+        this.personagem = new Personagem(VELOCIDADE_DESLOCAMENTO);
         this.personagem.Carregar();
 
         this.lobo = new Lobo();
         this.lobo.Carregar();
         
+        this.addKeyListener(this);
+
+        this.timer = new Timer(DELAY, this);
+        this.timer.start();
 
     }
 
@@ -37,5 +54,26 @@ public class Fase extends JPanel{
 
         g.dispose();
 
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        this.personagem.mover(e);
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        this.personagem.parar(e);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        this.personagem.atualizar();
+        repaint();
     }
 }
