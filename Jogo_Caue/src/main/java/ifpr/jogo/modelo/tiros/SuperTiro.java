d21292f24ed7118393ac8682fc92029430364dc1
CaueGrande <111818673+CaueGrande@users.noreply.github.com>
2023-10-23 20:31:58 -0300

    Update: SuperTiro

--- Jogo_Caue/src/main/java/ifpr/jogo/modelo/tiros/SuperTiro.java
+++ Jogo_Caue/src/main/java/ifpr/jogo/modelo/tiros/SuperTiro.java
@@ -4,24 +4,37 @@ import javax.swing.ImageIcon;
 
 public class SuperTiro extends AbstractTiro{
 
+    boolean explodido;
+
     public SuperTiro(int posicaoPersonagemX, int posicaoPersonagemY, int direcao) {
         super.setPosicaoX(posicaoPersonagemX);
         super.setPosicaoY(posicaoPersonagemY);
         super.setDirecao(direcao);
+        this.explodido = false;
         super.setVisivel(true);
         carregar();
     }
 
     @Override
     public void carregar() {
-        ImageIcon carregador = new ImageIcon(getClass().getResource("/granada.png"));
-        super.setImagem(carregador.getImage());
-        super.setAlturaImagem(super.getImagem().getHeight(null) );
-        super.setLarguraImagem(super.getImagem().getWidth(null) );
+        if (this.explodido == false) {
+            ImageIcon carregador = new ImageIcon(getClass().getResource("/granada.png"));
+            super.setImagem(carregador.getImage());
+            super.setAlturaImagem(super.getImagem().getHeight(null));
+            super.setLarguraImagem(super.getImagem().getWidth(null));
+
+        } else {
+            ImageIcon carregador = new ImageIcon(getClass().getResource("/explosao.png"));
+            super.setImagem(carregador.getImage());
+            super.setAlturaImagem(super.getImagem().getHeight(null));
+            super.setLarguraImagem(super.getImagem().getWidth(null));
+
+        }
     }
 
     public void explodir() {
-        super.setAlturaImagem(super.getAlturaImagem() + 200);
-        super.setLarguraImagem(super.getLarguraImagem() + 200);
+        this.explodido = true;
+        super.setPosicaoX(this.getPosicaoX() - 50);
+        super.setPosicaoY(this.getPosicaoY() - 50);
     }
 }
